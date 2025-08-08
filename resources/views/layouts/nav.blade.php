b30a4e4f78cb8f3843117f36544d8be990c70761
Leonardo Escalona <Leonardoescalona1408@gmail.com>
2025-08-08 19:30:34 -0400

    Implement structural updates and optimizations across multiple modules

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,10 +1,10 @@
 <nav class="bg-transparent shadow-none absolute top-0 left-0 w-full z-50">
     <div class="w-full max-w-9/10 mx-auto pt-10 flex justify-between items-center">
-        <!-- Logo -->
-        <a href="/" class="text-3xl font-bold text-primary flex items-center space-x-2">
-            <img src="{{ asset('assets/img/index/logo_navbar.jpg') }}" alt="Logo" class=" h-10 w-auto">
-        </a>
 
+        <a href="/" class="flex items-center h-16">
+            <img src="{{ asset('assets/img/index/logo-01.svg') }}" alt="Logo"
+                class="h-10 sm:h-12 md:h-14 lg:h-16 w-auto object-contain" />
+        </a>
         <!-- Desktop nav -->
         <ul class="hidden md:flex space-x-6 text-2xl text-gray-200 items-center relative">
             <li x-data="{ open: false }" class="relative">
@@ -31,31 +31,41 @@
                         @click="open = false">{{ __('Intelligent Chatbots') }}</a>
                 </div>
             </li>
-            <li>
-                <a href="{{ route('partners.index') }}" 
-                   class="{{ request()->routeIs('partners.index') ? 'text-orange-500 font-bold' : 'hover:text-orange-500' }}">
-                   <p>Partners</p>
-                </a>
-            </li>
-            <li><a href="{{ route('plans.index') }}" class="{{ request()->routeIs('plans.index') ? 'text-orange-500 font-bold' : 'hover:text-orange-500' }}"><p>{{__('Plans')}}</p></a></li>
-            <li><a href="{{ route('referred.index') }}" class="{{ request()->routeIs('referred.index') ? 'text-orange-500 font-bold' : 'hover:text-orange-500' }}"><p>Referidos</p></a></li>
-            <li><a href="{{ route('contact.index') }}"class="{{ request()->routeIs('contact.index') ? 'text-orange-500 font-bold' : 'hover:text-orange-500' }}"><p>{{__('Contact')}}</p></a></li>
-            
+            <li><a href="{{ route('partners.index') }}"
+                    class="{{ request()->routeIs('partners.index') ? 'text-orange-500 font-bold' : 'hover:text-orange-500' }}">
+                    <p>Partners</p>
+                </a></li>
+            <li><a href="{{ route('plans.index') }}"
+                    class="{{ request()->routeIs('plans.index') ? 'text-orange-500 font-bold' : 'hover:text-orange-500' }}">
+                    <p>{{ __('Plans') }}</p>
+                </a></li>
+            <li><a href="{{ route('referred.index') }}"
+                    class="{{ request()->routeIs('referred.index') ? 'text-orange-500 font-bold' : 'hover:text-orange-500' }}">
+                    <p>Referidos</p>
+                </a></li>
+            <li><a
+                    href="{{ route('contact.index') }}"class="{{ request()->routeIs('contact.index') ? 'text-orange-500 font-bold' : 'hover:text-orange-500' }}">
+                    <p>{{ __('Contact') }}</p>
+                </a></li>
+
             @if (request()->routeIs('home'))
                 <li>
-                    <a href="{{ route('register') }}" class="bg-orange-500 text-white px-6 py-1.5 rounded-xl hover:bg-transparent hover:border-2 hover:border-orange-500 hover:text-orange-500 transition">
+                    <a href="{{ route('register') }}"
+                        class="bg-orange-500 text-white px-6 py-1.5 rounded-xl hover:bg-transparent hover:border-2 hover:border-orange-500 hover:text-orange-500 transition">
                         {{ __('Registrar') }}
                     </a>
                 </li>
             @else
                 <li>
-                    <a href="{{ route('login') }}" class="bg-orange-500 text-white px-6 py-1.5 rounded-xl hover:bg-transparent hover:border-2 hover:border-orange-500 hover:text-orange-500 transition">
+                    <a href="{{ route('login') }}"
+                        class="bg-orange-500 text-white px-6 py-1.5 rounded-xl hover:bg-transparent hover:border-2 hover:border-orange-500 hover:text-orange-500 transition">
                         {{ __('Login') }}
                     </a>
                 </li>
-                
+
                 <li>
-                    <a href="{{ route('login') }}" class="bg-orange-500 text-white px-6 py-1.5 rounded-xl hover:bg-transparent hover:border-2 hover:border-orange-500 hover:text-orange-500 transition">
+                    <a href="{{ route('login') }}"
+                        class="bg-orange-500 text-white px-6 py-1.5 rounded-xl hover:bg-transparent hover:border-2 hover:border-orange-500 hover:text-orange-500 transition">
                         {{ __('Registrar') }}
                     </a>
                 </li>
@@ -72,25 +82,45 @@
 
     <!-- Mobile menu -->
     <div id="mobileMenu" class="hidden md:hidden bg-white text-gray-900 px-4 pt-4 pb-6 space-y-4 transition">
-        <details class="group">
+        <a href="{{ route('home') }}"
+            class="{{ request()->routeIs('home') ? 'text-orange-500 font-bold' : 'hover:text-orange-500' }}">
+            <p>Home</p>
+        </a>
+        <details class="group mb-0">
             <summary
-                class="cursor-pointer flex justify-between items-center font-semibold hover:text-primary transition-colors duration-200 hover:text-orange-500">
-                <span>{{ __('Products') }}</span>
+                class="cursor-pointer flex justify-between items-center font-normal hover:text-primary transition-colors duration-200 hover:text-orange-500">
+                <p>{{ __('Products') }}</p>
                 <svg class="ml-2 h-4 w-4 transition-transform duration-300 group-open:rotate-180" fill="none"
                     stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
                 </svg>
             </summary>
             <div class="ml-4 mt-2 space-y-1 animate-fade-in">
-                <a href="#tiibot" class="block hover:text-orange-500 transition-colors duration-200">{{__('Virtual voice agents')}}</a>
-                <a href="#tiicall" class="block hover:text-orange-500 transition-colors duration-200">{{__('Intelligent Chatbots')}}</a>
+                <a href="{{ route('tiicall.index') }}"
+                    class="{{ request()->routeIs('tiicall.index') ? 'text-orange-500 font-bold' : 'hover:text-orange-500' }}">
+                    <p>{{ __('Virtual voice agents') }}</p>
+                </a>
+                <a href="{{ route('tiibot.index') }}"
+                    class="{{ request()->routeIs('tiibot.index') ? 'text-orange-500 font-bold' : 'hover:text-orange-500' }}">
+                    <p>{{ __('Intelligent Chatbots') }}</p>
+                </a>
             </div>
         </details>
-        <a href="#servicios" class="block hover:text-orange-500">{{ __('Services') }}</a>
-        <a href="#integraciones" class="block hover:text-orange-500">Partners</a>
-        <a href="{{ route('plans.index') }}" class="block hover:text-orange-500">{{ __('Plans') }}</a>
-        <a href="{{ route('contact.index') }}" class="block hover:text-orange-500">{{ __('Contact') }}</a>
-            {{ __('Try it Free') }}
+        <a href="{{ route('partners.index') }}"
+            class="{{ request()->routeIs('partners.index') ? 'text-orange-500 font-bold' : 'hover:text-orange-500' }}">
+            <p>Partners</p>
+        </a>
+        <a href="{{ route('plans.index') }}"
+            class="{{ request()->routeIs('plans.index') ? 'text-orange-500 font-bold' : 'hover:text-orange-500' }}">
+            <p>{{ __('Plans') }}</p>
+        </a>
+        <a href="{{ route('referred.index') }}"
+            class="{{ request()->routeIs('referred.index') ? 'text-orange-500 font-bold' : 'hover:text-orange-500' }}">
+            <p>Referidos</p>
+        </a>
+        <a
+            href="{{ route('contact.index') }}"class="{{ request()->routeIs('contact.index') ? 'text-orange-500 font-bold' : 'hover:text-orange-500' }}">
+            <p>{{ __('Contact') }}</p>
         </a>
     </div>
 </nav>
