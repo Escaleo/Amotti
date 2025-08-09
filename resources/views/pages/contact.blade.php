8d4b0fbe14f435a15b3fe66e6e7d4bc60a1df2ea
Leonardo Escalona <Leonardoescalona1408@gmail.com>
2025-08-08 23:33:27 -0400

    fix: corregir estilos y atributos de imagen en las secciones de contacto y referidos

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 
     <!-- Header Section -->
-    <section class="relative pt-60 pb-30 mb-0">
+    <section class="relative lg:pt-60 pt-30 pb-30 mb-0">
         <!-- Background Image -->
         <img src="{{ asset('assets/img/contact/fondo.png') }}" alt="Hero Background"
             class="absolute inset-0 w-full h-full object-cover z-0">
@@ -38,7 +38,7 @@
             <!-- Right Content -->
             <div
                 class="relative lg:absolute lg:right-0 lg:top-1/2 lg:translate-y-[-50%] w-full lg:mr-[-110px] lg:w-1/2 flex justify-center lg:justify-end mt-12 z-10">
-                <img src="{{ asset('assets/img/contact/Pose.png') }}" alt="Hero Image"
+                <img src="{{ asset('assets/img/contact/pose.png') }}" alt="Hero Image"
                     class="w-full h-auto object-contain rotate-y-180">
             </div>
         </div>
@@ -75,10 +75,10 @@
                         <p class="text-2xl"><b>Teléfono:</b></p>
                     </div>
                     <div class="flex items-center gap-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="http://www.w3.org/2000/svg" width="40px"
-                            fill="#000">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px"
+                            fill="#00">
                             <path
-                                d="M140-160q-24 0-42-18t-18-42v-520q0-24 18-42t42-18h680q24 0 42 18t18 42v520q0 24-18 42t-42 18H140Zm340-302L140-685v465h680v-465L480-462Zm0-60 336-218H145l335 218ZM140-685v-55 520-465Z" />
+                                d="M146.67-160q-27 0-46.84-19.83Q80-199.67 80-226.67v-506.66q0-27 19.83-46.84Q119.67-800 146.67-800h666.66q27 0 46.84 19.83Q880-760.33 880-733.33v506.66q0 27-19.83 46.84Q840.33-160 813.33-160H146.67ZM480-454.67 146.67-670v443.33h666.66V-670L480-454.67Zm0-66.66 330.67-212H150l330 212ZM146.67-670v-63.33V-226.67-670Z" />
                         </svg>
                         <p class="text-2xl"><b>Correo:</b></p>
                     </div>
--- resources/views/pages/referred.blade.php
+++ resources/views/pages/referred.blade.php
@@ -190,10 +190,13 @@
                 <ul class="space-y-6 sm:space-y-8 text-base sm:text-lg md:text-xl lg:text-xl lg:ml-10">
                     <!-- Item 1 -->
                     <li class="flex items-start gap-4 hover:scale-105 transition-transform duration-300 ease-in-out">
-                        <svg xmlns="http://www.w3.org/2000/svg" height="40" width="40" fill="#000">
-                            <path
-                                d="m421-298 283-283-46-45-237 237-120-120-45 45 165 166Zm59 218q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-83 31.5-156t86-127Q252-817 325-848.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 82-31.5 155T763-197.5q-54 54.5-127 86T480-80Zm0-60q142 0 241-99.5T820-480q0-142-99-241t-241-99q-141 0-240.5 99T140-480q0 141 99.5 240.5T480-140Zm0-340Z" />
-                        </svg>
+                        <span class="shrink-0">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="48px"
+                                fill="#000">
+                                <path
+                                    d="m421-298 283-283-46-45-237 237-120-120-45 45 165 166Zm59 218q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-83 31.5-156t86-127Q252-817 325-848.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 82-31.5 155T763-197.5q-54 54.5-127 86T480-80Zm0-60q142 0 241-99.5T820-480q0-142-99-241t-241-99q-141 0-240.5 99T140-480q0 141 99.5 240.5T480-140Zm0-340Z" />
+                            </svg>
+                        </span>
                         <p class="text-base sm:text-lg lg:text-[22px] font-normal">
                             <b>Agente de Agendamiento y Calificación</b><br />
                             Atiende leads, los califica y agenda citas listas para cerrar
@@ -202,10 +205,13 @@
 
                     <!-- Item 2 -->
                     <li class="flex items-start gap-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" height="40" width="40" fill="#000">
-                            <path
-                                d="m421-298 283-283-46-45-237 237-120-120-45 45 165 166Zm59 218q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-83 31.5-156t86-127Q252-817 325-848.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 82-31.5 155T763-197.5q-54 54.5-127 86T480-80Zm0-60q142 0 241-99.5T820-480q0-142-99-241t-241-99q-141 0-240.5 99T140-480q0 141 99.5 240.5T480-140Zm0-340Z" />
-                        </svg>
+                        <span class="shrink-0">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="48px"
+                                fill="#000">
+                                <path
+                                    d="m421-298 283-283-46-45-237 237-120-120-45 45 165 166Zm59 218q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-83 31.5-156t86-127Q252-817 325-848.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 82-31.5 155T763-197.5q-54 54.5-127 86T480-80Zm0-60q142 0 241-99.5T820-480q0-142-99-241t-241-99q-141 0-240.5 99T140-480q0 141 99.5 240.5T480-140Zm0-340Z" />
+                            </svg>
+                        </span>
                         <p class="text-base sm:text-lg lg:text-[22px] font-normal">
                             <b>Agente de Recursos Humanos</b><br />
                             Automatiza selección, entrevistas y procesos de onboarding
@@ -214,10 +220,13 @@
 
                     <!-- Item 3 -->
                     <li class="flex items-start gap-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" height="40" width="40" fill="#000">
-                            <path
-                                d="m421-298 283-283-46-45-237 237-120-120-45 45 165 166Zm59 218q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-83 31.5-156t86-127Q252-817 325-848.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 82-31.5 155T763-197.5q-54 54.5-127 86T480-80Zm0-60q142 0 241-99.5T820-480q0-142-99-241t-241-99q-141 0-240.5 99T140-480q0 141 99.5 240.5T480-140Zm0-340Z" />
-                        </svg>
+                        <span class="shrink-0">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="48px"
+                                fill="#000">
+                                <path
+                                    d="m421-298 283-283-46-45-237 237-120-120-45 45 165 166Zm59 218q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-83 31.5-156t86-127Q252-817 325-848.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 82-31.5 155T763-197.5q-54 54.5-127 86T480-80Zm0-60q142 0 241-99.5T820-480q0-142-99-241t-241-99q-141 0-240.5 99T140-480q0 141 99.5 240.5T480-140Zm0-340Z" />
+                            </svg>
+                        </span>
                         <p class="text-base sm:text-lg lg:text-[22px] font-normal">
                             <b>Agente de Propuestas Comerciales</b><br />
                             Recibe requerimientos y genera propuestas al instante
